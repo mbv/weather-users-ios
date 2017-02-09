b6d680f53fc81f280d08df901e0ed60cd3818ee8
mbv <terehovk@ya.ru>
2017-02-09 22:01:54 +0300

    Initial Commit

---
+++ lab2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab2
+//
+//  Created by Konstantin Terehov on 2/9/17.
+//  Copyright © 2017 Konstantin Terehov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
